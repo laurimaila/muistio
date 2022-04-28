11fed2aef4565ecf88d566ade4a4029a01ebab36
laurimaila <laurikmaila@gmail.com>
2022-04-29 02:54:56 +0300

    async2

--- App.js
+++ App.js
@@ -5,10 +5,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const STORAGE_KEY = '@save_muistioState'
-
-
-
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -16,50 +12,33 @@ function getRandomInt(max) {
 var laskuri = 0;
 
 const Muistio = ({ navigation, route }) => {
-  const [muistioState, setMuistioState] = useState([{ "note": "Muistiinpano 1", "id": 1}]);
+  const [muistioState, setMuistioState] = useState([]);
 
-  const saveData = async () => {
-    try {
-      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(muistioState))
-      alert('Data successfully saved')
-    } catch (e) {
-      alert('Failed to save the data to the storage')
-    }
-  }
 
   
-
-  const readData = async () => {
-    try {
-      const muistiTila = await AsyncStorage.getItem(STORAGE_KEY)
-  
-      if (muistiTila !== null) {
-        muistiParsed = JSON.parse(muistiTila);
-        setMuistioState(muistiParsed)
-      }
-    } catch (e) {
-      alert('Failed to fetch the data from storage')
-    }
-  }
-
-  readData();
-
   useEffect(() => {
-    console.log("useEffect aktivoitu")
+    //console.log("useEffect aktivoitu")
     if (route.params?.uusiNote) {
       onPressButton()
     }
-  }, [laskuri]);
+  }, [laskuri]
+  );
+
+  useEffect(() => {
+    AsyncStorage.setItem('muistio', JSON.stringify(muistioState))
+  }, [muistioState]
+  );
 
   const onPressButton = () => {
     console.log("onPressButton aktivoitu")
-    console.log(muistioState);
+    getAsync();
+
     if (muistioState.some(e => e.note === route.params?.uusiNote)) {
       showAlert()
     } else {
       setMuistioState(muistioState => [...muistioState, { "note": route.params?.uusiNote, "id": getRandomInt(5000) }]);
-      saveData();
     }
+
   }
 
   return (
@@ -89,7 +68,7 @@ const NewNote = ({ navigation }) => {
         color="#ff4081"
         title="Tallenna muistiinpano"
         onPress={() => {
-          laskuri++; console.log("Count kasvatettu")
+          laskuri++;
           navigation.navigate({
             name: 'Muistio',
             params: { uusiNote: inputText },
